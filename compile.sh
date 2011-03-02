2524108a70a92c9e680fc7888aec8097e94bdd29
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-02 17:26:52 +0100

    simple compile script

---
+++ compile.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+ROOTDIR="$(pwd)"
+INCLUDE="$ROOTDIR"
+
+# $1 - target
+# $... - deps
+function checkdeps() {
+	local t="$1"
+	[ ! -e "$t" ] && return -1
+	while shift; do
+		[ "$1" != "" ] && [ ! -e "$1" ] && return -1
+		[ "$1" != "" ] && [ "$1" -nt "$t" ] && return -1
+	done
+	return 0
+}
+
+# $1 - dep-file
+function listdeps() {
+	sed -e "s/\\\\$//g" -e "s/.*\\.o://g" \
+		-e "s/^ *//g" -e "s/ *$//g" \
+		"$1"
+		#-e "s/^/\\\"/g" -e "s/$/\\\"/g" 
+}
+
+# $1 - cpp-file
+# will compile the o-file
+function srccompile() {
+	local f="$1"
+	local o="${f/.cpp/.o}"
+	[ -e "$f.deps" ] && checkdeps "$o" "$f" $(listdeps "$f.deps") && echo "uptodate: $o" && return 0
+	echo "compiling $o"
+	g++ -c -MMD -MF "$f.deps" -o "$o" -iquote "$INCLUDE" -g "$f" || exit -1
+}
+
+# $1 - cpp-file
+# will link the o-file and other in CppCodeBase together
+function srclink() {
+	local b="$1"
+	checkdeps "$b" $OBJS && echo "uptodate: $b" && return 0
+	echo "linking $b"
+	g++ $OBJS -o "$b" || exit -1
+}
+
+# compile all sources
+OBJS=""
+cd "."
+echo "> main"
+for f in *.cpp; do
+	srccompile "$f"
+	OBJS="$OBJS $(pwd)/${f/.cpp/.o}"
+done
+
+srclink "test-png.bin"
