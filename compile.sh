5ea326d0128d873d826aff0bfcf4b960eb523c2f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-05 17:55:57 +0100

    db-push tool (initial)

--- compile.sh
+++ compile.sh
@@ -52,7 +52,7 @@ function srclink() {
 	g++ $OBJS $o -o $b $2 || exit -1
 }
 
-BINS=("test-png-dumpchunks.cpp" "test-png-reader.cpp" "pnginfo.cpp")
+BINS=("test-png-dumpchunks.cpp" "test-png-reader.cpp" "pnginfo.cpp" "db-push.cpp")
 
 # compile all sources
 OBJS=()
---
+++ db-push.cpp
@@ -0,0 +1,11 @@
+/* tool to push some entry to the DB
+ * by Albert Zeyer, 2011
+ * code under LGPL
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+}
