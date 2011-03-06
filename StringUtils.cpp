3d8b8b2f9ce2adda43ce284b92abfedb60a9f7ff
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-06 14:49:13 +0100

    more stringutils

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -6,12 +6,15 @@
 #include "StringUtils.h"
 #include <cstdio>
 
+std::string hexString(char c) {
+	char buf[3];
+	sprintf(buf, "%02X", c);
+	return buf;
+}
+
 std::string hexString(const std::string& rawData) {
 	std::string ret;
-    for(size_t i = 0; i < rawData.size(); ++i) {
-		char buf[3];
-        sprintf(buf, "%02X", rawData[i]);
-		ret += buf;
-    }
+    for(size_t i = 0; i < rawData.size(); ++i)
+		ret += hexString(rawData[i]);
 	return ret;
 }
--- StringUtils.h
+++ StringUtils.h
@@ -7,7 +7,18 @@
 #define __AZ__STRINGUTILS_H__
 
 #include <string>
+#include <list>
 
+std::string hexString(char c);
 std::string hexString(const std::string& rawData);
+
+struct Matcher {
+	struct ExprPart {
+		std::string content;
+	};
+	std::list<ExprPart> expressionParts;
+	Matcher(const std::string& expression);
+	bool match(const std::string& s);
+};
 
 #endif
