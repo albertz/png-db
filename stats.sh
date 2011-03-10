d3fdcc8417d5297237280fc70a78c7af536e5b69
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-10 17:08:04 +0100

    stats script

---
+++ stats.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+DBFILE=db.pngdb
+SRCDIR="$HOME/screenies"
+[ "$1" != "" ] && SCRDIR="$1"
+
+zmodload zsh/stat
+sizesum=0
+
+./bin/db-list-dir | while read l; do
+	[[ "$l" =~ "^file: /(.*), .*, .*$" ]] && {
+		fn="$match"
+		size="$(stat -L +size $SRCDIR/$fn)"
+		sizesum=$(($sizesum + $size))
+	}
+done
+
+dbsize="$(stat -L +size $DBFILE)"
+
+echo "fs size: $(($sizesum / 1024 / 1024.0)) MB"
+echo "db size: $(($dbsize / 1024 / 1024.0)) MB"
