b5c5bbb219ccc859c55d24dfa63667c9a8b02835
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-10 17:13:56 +0100

    more stats

--- stats.sh
+++ stats.sh
@@ -6,16 +6,19 @@ SRCDIR="$HOME/screenies"
 
 zmodload zsh/stat
 sizesum=0
+numfiles=0
 
 ./bin/db-list-dir | while read l; do
 	[[ "$l" =~ "^file: /(.*), .*, .*$" ]] && {
 		fn="$match"
 		size="$(stat -L +size $SRCDIR/$fn)"
 		sizesum=$(($sizesum + $size))
+		numfiles=$(($numfiles + 1))
 	}
 done
 
 dbsize="$(stat -L +size $DBFILE)"
 
+echo "num files: $numfiles"
 echo "fs size: $(($sizesum / 1024 / 1024.0)) MB"
 echo "db size: $(($dbsize / 1024 / 1024.0)) MB"
